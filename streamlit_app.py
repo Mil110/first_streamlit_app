68e5fbce3d96060ab5a34ebcbc5217c3bb43e579
Mil110 <128577834+Mil110@users.noreply.github.com>
2023-03-25 22:08:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit
-streamlit.title('My Parent's New Healthy diner')
-                streamlit.title('My Breakfast')
+streamit.title("My Parent's New Healthy diner")
+                streamit.title('My Breakfast')
