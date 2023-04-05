dbfa8c2e07e3c82c4079f230e5e1620091d0a14c
Mil110 <128577834+Mil110@users.noreply.github.com>
2023-04-05 12:48:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,14 +24,15 @@ try:
   else:
     streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
+
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
